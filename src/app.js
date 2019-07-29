b487220d73f8ad1d0eedfeff6b64bcd8a7758d46
isheetsLingoport <isheets@lingoport.com>
2019-07-29 10:23:16 -0600

    i18n order status

--- src/app.js
+++ src/app.js
@@ -183,10 +183,13 @@ var showCart = () => {
 
 //adds some dummy orders to the history on startup
 let dummyOrders = () => {
-    let order1 = new Order(new Date(Date.UTC(2019, 5, 16)), 68500); //$NON-NLS-L$
-    let order2 = new Order(new Date(Date.UTC(2019, 6, 9)), 900); //$NON-NLS-L$
-    orderHistory.push(order2);
+    let now = new Date(); //$NON-NLS-L$
+    var twoDays = now - 1000 * 60 * 60 * 24 * 2;
+    var fiveDays = now - 1000 * 60 * 60 * 24 * 5;
+    let order2 = new Order(900, new Date(fiveDays)); //$NON-NLS-L$
+    let order1 = new Order(68500, new Date(twoDays)); //$NON-NLS-L$
     orderHistory.push(order1);
+    orderHistory.push(order2);
 }
 
 let featuredProducts = [];
@@ -256,7 +259,6 @@ const router = async () => {
 
     // Parse the URL and if it has an id part, change it with the string ":id"
     let parsedURL = (request.resource ? './' + request.resource : './') + (request.id ? '/:id' : '') + (request.verb ? './' + request.verb : '')
-    console.log(parsedURL);
     
     // Get the page from our hash of supported routes.
     // If the parsed URL is not in our list of supported routes, select the 404 page instead
