3b568defa429fc359e769193b4e3b0515f77e546
isheetsLingoport <isheets@lingoport.com>
2019-08-02 12:50:25 -0600

    updated version links

--- src/views/components/Hamburger.js
+++ src/views/components/Hamburger.js
@@ -76,10 +76,10 @@ var switchVersion = (e) => {
     let selectedOption = event.target.value;
 
     if (selectedOption == "IQA") {
-        window.location.href = `http://34.204.74.162:8080/RebelOutfitters.IQA/${parsedURL}`;
+        window.location.href = `http://rebeloutfitters.lingoport.com/RebelOutfitters.IQA/${parsedURL}`;
     }
     else if (selectedOption == "good") {
-        window.location.href = `http://34.204.74.162:8080/RebelOutfitters.i18n/${parsedURL}`;
+        window.location.href = `http://rebeloutfitters.lingoport.com/RebelOutfitters.i18n/${parsedURL}`;
     }
 }
 
