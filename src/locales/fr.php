d2829a168e35456c20d33d5e74532d477ebd899c
Jenkins Automation Server <support@lingoport.com>
2021-01-18 20:27:15 +0000

    Lingoport Import. RebelOutfitters.Vanilla - Kit 3: fr

---
+++ src/locales/fr.php
@@ -0,0 +1,7 @@
+<?php
+
+$Definition['All Groups'] = 'Tous les groupes';
+$Definition['Ask questions about the biggest family history conference in the world.'] = 'Posez des questions sur la plus grande conférence d’histoire familiale au monde.';
+$Definition['Welcome to FamilySearch Community!'] = 'Bienvenue dans FamilySearch Community!';
+$Definition['Dollar Parameters ${username} $username Groups'] = 'Paramètres dollar ${username} $username groupes';
+$Definition['Percent Parameters %{username} %username Groups'] = 'Paramètres dollar %{username} %username de noms d’utilisateur';
