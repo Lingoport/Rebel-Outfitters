bcd1f1d8c8cbf65cfb4e8ef533d6c90dc2c20d13
Olivier Libouban <oliboubanLingoport@users.noreply.github.com>
2021-01-18 12:34:49 -0700

    Create en.php

---
+++ src/locales/en.php
@@ -0,0 +1,7 @@
+<?php
+
+$Definition['All Groups'] = 'All Groups';
+$Definition['Ask questions about the biggest family history conference in the world.'] = 'Ask questions about the biggest family history conference in the world.';
+
+$Definition['Welcome to FamilySearch Community!'] = 'Welcome to FamilySearch Community!';
+
