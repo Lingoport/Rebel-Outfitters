67d0a8d3b4485c4ea7b3a11f5442aa0f21bd8766
Olivier Libouban <oliboubanLingoport@users.noreply.github.com>
2021-01-18 12:36:29 -0700

    Update en.php

--- src/locales/en.php
+++ src/locales/en.php
@@ -5,3 +5,7 @@ $Definition['Ask questions about the biggest family history conference in the wo
 
 $Definition['Welcome to FamilySearch Community!'] = 'Welcome to FamilySearch Community!';
 
+$Definition['Dollar Parameters ${username} $username Groups'] = 'Dollar Parameters ${username} $username Groups';
+$Definition['Percent Parameters %{username} %username Groups'] = 'Dollar Parameters %{username} %username Groups';
+
+
